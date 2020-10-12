bd3aff5ea3a5f7ea225c098b4950da8c31528d37
Myles Eftos <myles@madpilot.com.au>
2020-10-12 13:29:12 +1100

    REmoving need for README atm

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="intesis_wmp",
     version="0.0.1",
     author="Myles Eftos",
     author_email="myles@madpilot.com.au",
     description="Python library for talking to Intesis devices using WMP",
-    long_description=long_description,
+    long_description="Python library for talking to Intesis devices using WMP",
     long_description_content_type="text/markdown",
     url="https://github.com/madpilot/pyintesiswmp",
     packages=setuptools.find_packages(),
