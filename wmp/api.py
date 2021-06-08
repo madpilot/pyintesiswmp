2a5039c63bd9b78e53aac8d4095600777e7352b0
Myles Eftos <myles@madpilot.com.au>
2021-06-08 22:22:12 +1000

    Removing command file, as it's not required. Adding type hints to api

--- wmp/api.py
+++ wmp/api.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Union
+from wmp.handler import CfgResult, CnfResult, IdResult, InfoResult, LimitsResult, PingResult
 from wmp.asyncio import Asyncio
 
 
@@ -6,36 +7,36 @@ class API:
     def __init__(self, protocol: Asyncio):
         self._protocol = protocol
 
-    def ping(self):
+    def ping(self) -> PingResult:
         return self._protocol.send("PING")
 
-    def id(self):
+    def id(self) -> IdResult:
         return self._protocol.send("ID")
 
-    def info(self):
+    def info(self) -> List[InfoResult]:
         return self._protocol.send("INFO")
 
-    def set(self, ac_num: int, function: str, value: str):
+    def set(self, ac_num: int, function: str, value: str) -> bool:
         return self._protocol.send("SET,%i:%s,%s" % (ac_num, function, value))
 
-    def get(self, ac_num: int, function: str):
+    def get(self, ac_num: int, function: str) -> CnfResult:
         return self._protocol.send("GET,%i:%s" % (ac_num, function))
 
     def login(self, password: str):
         return self._protocol.send("LOGIN:%s" % (password))
 
-    def logout(self):
+    def logout(self) -> bool:
         return self._protocol.send("LOGOUT")
 
-    def send_config(self, item: str, value: str):
+    def send_config(self, item: str, value: str) -> bool:
         return self._protocol.send("CFG:%s,%s" % (item, value))
 
-    def get_config(self, item: str):
+    def get_config(self, item: str) -> CfgResult:
         return self._protocol.send("CFG:%s" % (item))
 
-    def set_limits(self, function: str, range: List[str]):
+    def set_limits(self, function: str, range: List[Union[str, int]]) -> bool:
         range_str = map(lambda s: str(s), range)
         return self._protocol.send("LIMITS:%s,[%s]" % (function, ",".join(range_str)))
 
-    def get_limits(self, function: str):
+    def get_limits(self, function: str) -> LimitsResult:
         return self._protocol.send("LIMITS:%s" % (function))
